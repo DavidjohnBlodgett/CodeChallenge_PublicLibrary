54b002275680c9b6217973559711eb18d84bc802
Davidjohn Blodgett <davidjohn.blodgett@gmail.com>
2015-03-28 11:34:24 -0700

    added lib obj functionality and name attr for other objs

--- PublicLibrary.py
+++ PublicLibrary.py
@@ -3,37 +3,49 @@ __author__ = 'Davidjohn Blodgett'
 
 class Library:
     # Attributes
+    __name = None
     __Shelves = []
 
     # Constructor
-    def __init__(self):
-        print("A Library created!")
+    def __init__(self, name):
+        self.__name = name
+        print("A Library created called %s!" % (self.__name))
 
     # Object Methods
+    def get_name(self):
+        return self.__name
+
     def add_shelf(self, Shelf):
         self.__Shelves.append(Shelf)
 
     def remove_shelf(self, Shelf):
         self.__Shelves.remove(Shelf)
 
+    #TODO def list_shelves()
+
     def list_books(self):
         if len(self.__Shelves) <= 0:
                 print("No Shelves found!")
         else:
             for x in self.__Shelves:
-                print("Shelf %d has the below books:" % ((x + 1)))
-                self.__Shelves[x].list_books()
+                print("Shelf %s has the below books:" % (x.get_name()))
+                x.list_books()
 
 
 class Shelf:
     # Attributes
+    __name  = None
     __Books = []
 
     # Constructor
-    def __init__(self):
-        print("A Shelf has been created!")
+    def __init__(self, name):
+        self.__name = name
+        print("%s has been created!" % (self.__name))
 
     # Object Methods
+    def get_name(self):
+        return self.__name
+
     def add_book(self, newBook):
         self.__Books.append(newBook)
         print("%s has been placed on the shelf!" % (newBook.get_title()))
@@ -46,7 +58,6 @@ class Shelf:
             print("No books found!")
         else:
             for x in self.__Books:
-                #a_book = self.__Books[x]
                 print("Book Title is: ", x.get_title())
 
 class Book:
@@ -82,8 +93,13 @@ if(__name__ == "__main__"):
 
     # create objects...
     mybook  = Book('Learn Python!!')
-    myshelf = Shelf()
-    mylib   = Library()
+    myshelf = Shelf("myshelf")
+    mylib   = Library("Seattle Public Library")
+
+    # add shelves to lib
+    print("\nAdding existing shelves to library")
+    mylib.add_shelf(myshelf)
+
 
     # test what happens when nothing is in objects...
     print("\nTest when a shelf or library is empty:")
@@ -103,4 +119,6 @@ if(__name__ == "__main__"):
     print("\nAttempting to list books on shelf:")
     myshelf.list_books()
 
+    print("\nAttempting to have Lib list all books:")
+    mylib.list_books()
 
